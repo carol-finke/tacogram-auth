814818e7a484792b383186fd383a123f3d8a58ff
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-05-21 01:30:06 +0000

    may 20 updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
-    redirect_to "/login"
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully."
+      redirect_to posts_path
+    else
+      flash[:alert] = "Invalid email or password."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully."
+    redirect_to login_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_02_28_013050) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_012736) do
   create_table "posts", force: :cascade do |t|
     t.string "body"
     t.string "image"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2022_02_28_013050) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
